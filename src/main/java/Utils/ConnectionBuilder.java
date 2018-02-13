7a06d6a2a25f35a7424b237be495578579943d36
Tatchai Samutsopakul <iridescentz67@gmail.com>
2018-02-13 11:42:00 +0700

    [Edit] add code ConnectionBuilder

--- src/main/java/Utils/ConnectionBuilder.java
+++ src/main/java/Utils/ConnectionBuilder.java
@@ -5,6 +5,29 @@
  */
 package Utils;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ConnectionBuilder {
-    
+
+    public static Connection getConnection() throws SQLException, URISyntaxException, ClassNotFoundException {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            URI dbUri = new URI(System.getenv("DATABASE_URL"));
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:mysql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+            con = DriverManager.getConnection(dbUrl, username, password);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnectionBuilder.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return con;
+    }
 }
