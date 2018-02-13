8dbc6837a2d25764e58dc6bc2e46dfba82b2a87a
tubtim13 <thapthim.kuphku@mail.kmutt.ac.th>
2018-02-13 12:09:35 +0700

    [Edit]Add Dummy Model

--- src/main/java/Model/DummyModel.java
+++ src/main/java/Model/DummyModel.java
@@ -5,10 +5,49 @@
  */
 package Model;
 
+import Utils.ConnectionBuilder;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author KN
  */
 public class DummyModel {
+    public DummyModel(){
     
+    }
+ public String queryDemo() throws SQLException{
+        try {
+            String demo = "";
+            Connection con = ConnectionBuilder.getConnection();
+            Statement st;
+            ResultSet rs = null;
+            try {
+                st = con.createStatement();
+                rs = st.executeQuery("SELECT * FROM TestLoadData");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if(rs != null){
+                while(rs.next())
+                    demo = rs.getString("data");
+            }
+            return demo;
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(DummyModel.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DummyModel.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+ 
+    public static void main(String[] args) throws SQLException {
+        System.out.println((new DummyModel()).queryDemo());
+    }
 }
